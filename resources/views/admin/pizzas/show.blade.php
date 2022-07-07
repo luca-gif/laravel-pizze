d71a86343f8217e5b57cd44687e04cd9950055d0
luca-gif <luca.89m@gmail.com>
2022-07-07 17:55:24 +0200

    modificata pagina show

--- resources/views/admin/pizzas/show.blade.php
+++ resources/views/admin/pizzas/show.blade.php
@@ -2,25 +2,29 @@
 
 @section('content')
     <div class="container">
-        <ul>
-            <li>Nome Pizza: {{ $pizza->name }}</li>
-            <li>Ingredienti: {{ $pizza->description }}</li>
-            <li>Prezzo: € {{ $pizza->price }}</li>
+        <div class="row">
+            <div class="card col-6 offset-3 p-4">
+                <ul>
+                    <li>Nome Pizza: {{ $pizza->name }}</li>
+                    <li>Ingredienti: {{ $pizza->description }}</li>
+                    <li>Prezzo: € {{ $pizza->price }}</li>
 
-            @if ($pizza->isVegetarian)
-                <li>
-                    Questa pizza è vegetariana
-                </li>
-            @endif
+                    @if ($pizza->isVegetarian)
+                        <li>
+                            Questa pizza è vegetariana
+                        </li>
+                    @endif
 
-            @if ($pizza->popularity)
-                <li>
-                    Popolarità: {{ $pizza->popularity }}
-                </li>
-            @endif
-        </ul>
+                    @if ($pizza->popularity)
+                        <li>
+                            Popolarità: {{ $pizza->popularity }}
+                        </li>
+                    @endif
+                </ul>
 
-        <a class="btn btn-dark" href="{{ route('admin.pizze.index') }}">Back</a>
+                <a class="btn btn-dark" href="{{ route('admin.pizze.index') }}">Back</a>
+            </div>
+        </div>
 
     </div>
 @endsection
